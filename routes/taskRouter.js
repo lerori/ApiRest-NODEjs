defb0b9d106cdd0befc4c15c07c4ce409f6c70d7
Leo Riccieri <leoriccieri@gmail.com>
2019-10-03 22:00:01 -0300

    Update Node 6.19

--- routes/taskRouter.js
+++ routes/taskRouter.js
@@ -1,72 +1,70 @@
-var express = require('express');
-var Task = require('../models/taskModel');
+var express = require("express");
+var Task = require("../models/taskModel");
 
-var taskRouter = express.Router();
+var taskRouter = express.Router(); //passing the data to DB
 
-taskRouter.route('/')
-	//Getting tasks from database
-    .get((req, res) => {
-        Task.find({}, (err, tasks) => {
-            res.json(tasks);
-        })  
-    })
-    //Adding content to the database
-    .post((req, res) => {
-        let task = new Task(req.body);
-        task.save();
-        res.status(201).send(task);
-    })
+taskRouter
+  .route("/")
+  //Getting tasks from database
+  .get((req, res) => {
+    Task.find({}, (err, tasks) => {
+      res.json(tasks);
+    });
+  })
+  //Adding content to the database
+  .post((req, res) => {
+    let task = new Task(req.body);
+    task.save();
+    res.status(201).send(task);
+  });
 
 //middleware
-taskRouter.use('/:taskId', (req, res, next)=>{
-	Task.findById(req.params.taskId, (err, task)=>{
-		if(err)
-			res.status(500).send('Status 500 - '.err)
-		else {
-			req.task =task;
-			next()
-		}
-	})
-})
+taskRouter.use("/:taskId", (req, res, next) => {
+  Task.findById(req.params.taskId, (err, task) => {
+    if (err) res.status(500).send("Status 500 - ".err);
+    else {
+      req.task = task;
+      next();
+    }
+  });
+});
 
-taskRouter.route('/:taskId')
-    .get((req, res) => {
-        res.json(req.task)
-    })
-    //Editing tasks in the database
-    .put((req,res) => {
-        req.task.title = req.body.title
-        req.task.description = req.body.description
-        req.task.due_date = req.body.due_date
-        req.task.completed = req.body.completed
-        //req.task.created_at = req.body.created_at
-        req.task.updated_at = req.body.updated_at
-        req.task.save()
-        res.json(req.task);
-    })
-    //Editing task properties
-    .patch((req,res)=>{
-        
-        if(req.body._id){
-           delete req.body._id
-        }
-        for( let p in req.body ){
-            task[p] = req.body[p]
-        }
-        req.task.save()
-        res.json(req.task)
-    })
-    //delete task
-    .delete((req,res)=>{
-        
-        req.task.remove(err => {
-            if(err){
-                res.status(500).send(err)
-            }
-            else{
-                res.status(204).send('removed')
-            }
-        })
-    })
+taskRouter
+  .route("/:taskId")
+  .get((req, res) => {
+    res.json(req.task);
+  })
+  //Editing tasks in the database
+  .put((req, res) => {
+    req.task.title = req.body.title;
+    req.task.description = req.body.description;
+    req.task.due_date = req.body.due_date;
+    req.task.completed = req.body.completed;
+    //req.task.created_at = req.body.created_at
+    req.task.updated_at = req.body.updated_at;
+    req.task.save();
+    res.json(req.task);
+  })
+  //Editing task properties
+  .patch((req, res) => {
+    if (req.body._id) {
+      delete req.body._id;
+    }
+    for (let p in req.body) {
+      req.taskclear[p] = req.body[p];
+    }
+    req.task.save();
+    res.json(req.task);
+  })
+  //delete task
+  .delete((req, res) => {
+    req.task.remove(err => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(204).send("removed");
+      }
+    });
+  });
 
 module.exports = taskRouter;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var taskRouter = require('./routes/taskRouter');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 var app = express();
 
@@ -9,19 +8,23 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API routes 
-app.use('/api/tasks', taskRouter);
-
 // Connecting to the database
 var port = process.env.PORT || 3100;
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://prueba:prueba2018@ds133642.mlab.com:33642/lerori',{ useNewUrlParser: true })
-    .then(() => {        
-        console.log("The connection to the database was successful")    
-        //Running the server
-		app.listen(port, () => {
-    		console.log(`Server running in http://localhost:${port}`)
-		});
-    })
-    //If it does not connect correctly we show the error 
-    .catch(err => console.log(err));
+mongoose
+  .connect("mongodb://prueba:prueba2018@ds133642.mlab.com:33642/lerori", {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log("The connection to the database was successful");
+    //Running the server
+    app.listen(port, () => {
+      console.log(`Server running in http://localhost:${port}`);
+    });
+  })
+  //If it does not connect correctly we show the error
+  .catch(err => console.log(err));
+
+// API routes
+var tRouter = require("./routes/taskRouter");
+app.use("/api/tasks", tRouter);
